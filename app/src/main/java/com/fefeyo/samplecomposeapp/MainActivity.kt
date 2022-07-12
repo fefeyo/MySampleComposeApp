48aa18defcf330294448d23c25467f7658af7a78
fefeyo <imanity.fefe@gmail.com>
2022-07-12 17:07:26 +0900

    ボタン付きリスト作成

---
+++ app/src/main/java/com/fefeyo/samplecomposeapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.fefeyo.samplecomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fefeyo.samplecomposeapp.ui.theme.SampleComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      SampleComposeAppTheme {
+//        Surface(color = MaterialTheme.colors.background) {
+//          Greeting("Android")
+//        }
+        MyApp()
+      }
+    }
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun MyApp(names: List<String> = listOf("Apple", "Banana", "Chocolate")) {
+  Column {
+    for (name in names) {
+      Greeting(name)
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Surface(
+    color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+    Row(modifier = Modifier.padding(24.dp)) {
+      Column(modifier = Modifier.weight(1f)) {
+        Text(text = "Hello")
+        Text(text = "$name!")
+      }
+      OutlinedButton(onClick = { /*TODO*/ }) {
+        Text(text = "Show more")
+      }
+    }
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+  SampleComposeAppTheme {
+    MyApp()
+  }
+}
