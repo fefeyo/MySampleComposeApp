3d472596c783329a260340b31280aa5d8938cb89
fefeyo <imanity.fefe@gmail.com>
2022-07-13 16:16:42 +0900

    スタートページの作成、ボタンからのメインページへ遷移できるように

--- app/src/main/java/com/fefeyo/samplecomposeapp/MainActivity.kt
+++ app/src/main/java/com/fefeyo/samplecomposeapp/MainActivity.kt
@@ -3,15 +3,22 @@ package com.fefeyo.samplecomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,39 +29,21 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       SampleComposeAppTheme {
-//        Surface(color = MaterialTheme.colors.background) {
-//          Greeting("Android")
-//        }
         MyApp()
       }
     }
   }
 }
 
-@Preview(showBackground = true, widthDp = 320)
 @Composable
-private fun MyApp(names: List<String> = listOf("Apple", "Banana", "Chocolate")) {
-  Column {
-    for (name in names) {
-      Greeting(name)
-    }
-  }
-}
+private fun MyApp() {
 
-@Composable
-fun Greeting(name: String) {
-  Surface(
-    color = MaterialTheme.colors.primary,
-    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-    Row(modifier = Modifier.padding(24.dp)) {
-      Column(modifier = Modifier.weight(1f)) {
-        Text(text = "Hello")
-        Text(text = "$name!")
-      }
-      OutlinedButton(onClick = { /*TODO*/ }) {
-        Text(text = "Show more")
-      }
-    }
+  var shouldShowOnboarding: Boolean by remember { mutableStateOf(true) }
+
+  if (shouldShowOnboarding) {
+    OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+  } else {
+    Greetings()
   }
 }
 
@@ -63,5 +52,64 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
   SampleComposeAppTheme {
     MyApp()
+  }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+  Surface {
+    Column(
+      modifier = Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text("Welcome to the Basics Codelab!")
+      Button(
+        modifier = Modifier.padding(vertical = 24.dp),
+        onClick = onContinueClicked
+      ) {
+        Text("Continue")
+      }
+    }
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+  SampleComposeAppTheme {
+    OnboardingScreen({})
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  val expanded = remember { mutableStateOf(false) }
+  val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+  Surface(
+    color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+  ) {
+    Row(modifier = Modifier.padding(24.dp)) {
+      Column(modifier = Modifier
+        .weight(1f)
+        .padding(bottom = extraPadding)) {
+        Text(text = "Hello")
+        Text(text = "$name!")
+      }
+      OutlinedButton(onClick = { expanded.value = expanded.value.not() }) {
+        Text(if (expanded.value) "Show less" else "Show more")
+      }
+    }
+  }
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("Apple", "Banana", "Chocolate")) {
+  Column(modifier = Modifier.padding(vertical = 4.dp)) {
+    for (name in names) {
+      Greeting(name = name)
+    }
   }
 }
